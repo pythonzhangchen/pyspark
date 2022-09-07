cd2ab14607b0ff511d57d6c13e051d3fcc824624
-_- <429478948@qq.com>
2022-09-07 14:51:43 +0800

    RDD

---
+++ 01_RDD/defs_21.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/9/5 23:49 
+# @Author : chen.zhang 
+# @File : 19_operators_demo.py
+
+
+def city_with_category(data):
+    return data['areaName'] + '_' + data['category']
